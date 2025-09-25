e6c1b0691cc70b21e560ba9830cb59bbb63f5126
Bilal-Choudhry-Git <muhammadbilal.choudhry@edu.sait.ca>
2025-09-25 10:39:54 -0600

    Changes made According to instructors feedback

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,9 +1,14 @@
-export default function Item({ name, quantity, category }) {
+export default function Item({ item }) {
+  const { name, quantity, category } = item; 
+
   return (
-    <li>
-      <div className="text-lg font-semibold text-gray-800">{name}</div>
-      <div className="text-sm font-semibold text-gray-600">Quantity: {quantity}</div>
-      <div className="text-sm font-semibold text-gray-600">Category: {category}</div>
-    </li>
+    <div className="flex justify-center items-center"> 
+      <ul className="bg-neutral-700 text-white p-2 mb-3 rounded-md shadow-md w-80">
+        <li className="text-lg font-bold text-left" >
+          <p>{name}</p>
+          <p className="text-sm font-normal">Buy {quantity} in {category}</p>
+        </li>
+      </ul>
+    </div>
   );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,11 +2,10 @@ import ItemList from "./item-list";
 
 export default function Page() {
   return (
-    <main className="p-6 max-w-2xl mx-auto bg-gray-50 rounded-lg ">
-      <h1 className="text-2xl font-bold text-center mb-6 text-gray-800">
+      <main className = "text-center font-bold text-white text-2xl">
         Shopping List
-      </h1>
+      
       <ItemList />
-    </main>
+      </main>
   );
 }
